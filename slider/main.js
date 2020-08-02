efe0d0cd7d3016c3b38d1e3c391ba1c3d2a1dd66
Ivan <zeesnippi@gmail.com>
2020-08-02 19:28:08 +0300

    4

--- slider/main.js
+++ slider/main.js
@@ -1,14 +1,24 @@
+//получаем кнопки
 document.getElementById('slider_button-left').onclick = sliderLeft;
 document.getElementById('slider_button-right').onclick = sliderRight;
 
+//хардкодим значение лефт
 let left = 0;
 
+//функция листать влево
 function sliderLeft() {
+    //получаем полосу слайдера
     var sliderBody = document.getElementById('slider-body');
+
+    //задаем значение переменной
     left = left - 200;
+
+    //проврка если слайдер в конце
     if (left < -1400) {
         left = 0;
     }
+
+    //прокрутка слайда
     sliderBody.style.left = left + 'px';
 }
 
