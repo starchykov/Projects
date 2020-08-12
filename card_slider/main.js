88c336433f6e454f6a2b0a1933ad34facd1d3937
Ivan <zeesnippi@gmail.com>
2020-08-13 01:17:40 +0300

    5

---
+++ card_slider/main.js
@@ -0,0 +1,28 @@
+let minImg = document.querySelectorAll('.img-min');
+let maxImg = document.getElementsByClassName('img-max');
+
+
+for (i = 0; i < minImg.length; i++) {
+
+    minImg[i].setAttribute('class', 'img-min');
+
+    minImg[i].addEventListener('click', (element) => {
+        newSrc = element.target;
+        setMax(newSrc);
+    });
+
+    minImg[0].classList = 'active';
+}
+
+setMax = (newSrc) => {
+
+    for (i = 0; i < minImg.length; i++) {
+        minImg[i].setAttribute('class', 'img-min');
+
+    }
+
+    if (newSrc.className === 'img-min') {
+        maxImg[0].src = newSrc.src;
+        newSrc.setAttribute('class', 'active')
+    }
+};
