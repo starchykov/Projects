ce37e3ca2e67e4cd3e6dddf463d9e2d50a89ba4c
Ivan <zeesnippi@gmail.com>
2021-10-22 18:35:41 +0300

    Object_slider some general improvements.

--- obj_slider/js/slider.js
+++ obj_slider/js/slider.js
@@ -4,63 +4,75 @@ let Slider = function () {
     this.image = null;
     this.prev = null;
     this.next = null;
+    this.indicators;
 
     this.init = function (el) {
 
-        let element = document.getElementById(el);
+        let viewContainer = document.getElementById(el);
 
-        this.image = element.querySelector('.slideImage');
-        this.prev = element.querySelector('.prev');
-        this.next = element.querySelector('.next');
+        this.image = viewContainer.querySelector('.slideImage');
+        this.prev = viewContainer.querySelector('.prev');
+        this.next = viewContainer.querySelector('.next');
+        this.indicators = viewContainer.querySelector('.indicators');
 
-        //context on init moment
+        // Context on init moment
         let that = this;
-        this.prev.addEventListener("click", (e) => {
-            that.slidePrev(e)
-        });
-        this.next.addEventListener("click", (e) => {
-            that.slideNext(e)
-        });
-        //massive of images
-        this.imageUrls.push('https://avatars.mds.yandex.net/get-pdb/70729/d65811a6-1ad6-4c3b-80b1-2f4e4a9fdd52/s1200');
-        this.imageUrls.push('https://avatars.mds.yandex.net/get-pdb/163339/f8cffa0c-e554-403c-9399-150592a6d96f/s1200');
-        this.imageUrls.push('https://avatars.mds.yandex.net/get-pdb/750514/84cb3f69-5a02-445e-bc31-5efc67e63df1/s1200');
-        this.imageUrls.push('https://avatars.mds.yandex.net/get-pdb/51720/d2d3fd99-5446-47d5-99cb-131f36f83494/s1200');
+        this.prev.addEventListener("click", (e) => that.slidePrev(e));
+        this.next.addEventListener("click", (e) => that.slideNext(e));
+
+        // Set image srs to array
+        this.imageUrls.push('https://static.kulturologia.ru/files/u18172/1817201340.jpg');
+        this.imageUrls.push('https://vjoy.cc/wp-content/uploads/2019/07/1-1.jpg');
+        this.imageUrls.push('https://5sfer.com/wp-content/uploads/2015/08/8ipwnn.jpg');
+        this.imageUrls.push('https://jizn.info/wp-content/uploads/2020/08/1.jpg');
+
+        // Set current image src attribute to <img> tag
         this.image.src = this.imageUrls[this.currentImage];
+
+        let indicatorItems = []
+        this.imageUrls.forEach(() => indicatorItems = [...indicatorItems, '<div></div>'])
+        this.indicators.innerHTML = indicatorItems.join('');
+        this.indicators.childNodes[0].classList.toggle('active');
+
+
+        // Disable ability to click prev button
         this.prev.disabled = true;
         this.prev.setAttribute('class', 'disabled')
     };
 
-    this.slidePrev = function () {
+    this.slidePrev = () => {
         this.currentImage--;
-        if (this.currentImage < 0) {
+
+        // Set active indicator
+        this.indicators.childNodes[this.currentImage + 1].classList.toggle('active');
+        this.indicators.childNodes[this.currentImage].classList.toggle('active');
+
+        // Change view image
+        this.image.src = this.imageUrls[this.currentImage];
+        this.next.disabled = false;
+        this.next.removeAttribute('class')
+
+        if (this.currentImage <= 0) {
             this.prev.disabled = true;
             this.prev.setAttribute('class', 'disabled')
-        } else {
-            this.image.src = this.imageUrls[this.currentImage];
-            this.next.disabled = false;
-            this.next.removeAttribute('class')
         }
     };
-    this.slideNext = function () {
+
+    this.slideNext = () => {
         this.currentImage++;
-        if (this.currentImage >= this.imageUrls.length) {
+
+        // Set active indicator
+        this.indicators.childNodes[this.currentImage - 1].classList.toggle('active');
+        this.indicators.childNodes[this.currentImage].classList.toggle('active');
+
+        // Change view image
+        this.image.src = this.imageUrls[this.currentImage];
+        this.prev.disabled = false;
+        this.prev.removeAttribute('class');
+
+        if (this.currentImage >= this.imageUrls.length - 1) {
             this.next.disabled = true;
             this.next.setAttribute('class', 'disabled')
-        } else {
-            this.image.src = this.imageUrls[this.currentImage];
-            this.prev.disabled = false;
-            this.prev.removeAttribute('class');
         }
     }
 };
-
-
-let sliderFactory = {
-        createNewSlider: () => {
-            let newSlider = new Slider();
-            return newSlider;
-
-        }
-    }
-;
